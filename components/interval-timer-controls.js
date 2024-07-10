d29c6f023dbaf0feee3afc1cb2aefd064c964ec1
tomyo <tomyhayes@gmail.com>
2024-07-10 02:22:07 -0300

    Implement exit button in timer controls

--- components/interval-timer-controls.js
+++ components/interval-timer-controls.js
@@ -3,6 +3,9 @@ customElements.define(
   class extends HTMLElement {
     constructor() {
       super().innerHTML = /*html*/ `
+      <a class="button" name="exit" href="/">
+        <svg viewBox="0 0 1024 1024" fill="currentColor" with="50px" height="40px" xmlns="http://www.w3.org/2000/svg" fill="#000000" ><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g ><path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path><path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" ></path></g></svg>
+      </a>
       <button
             name="prev"
             title="Previous activity"
