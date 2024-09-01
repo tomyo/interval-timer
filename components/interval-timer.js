0d5cc145237d24ad44f916273866d779fe298545
tomyo <tomyhayes@gmail.com>
2024-09-01 11:47:25 -0300

    Fix hack to avoid suspension

--- components/interval-timer.js
+++ components/interval-timer.js
@@ -153,7 +153,7 @@ customElements.define(
      * @returns {void}
      */
     tick = () => {
-      playBeep({ volume: 0, length: 0.0001 }); // Hack to avoid page to be suspended while ticking
+      playBeep({ volume: 0.0001, length: 0.0001 }); // Hack to avoid page to be suspended while ticking
 
       // A second has passed
       let m = Number(this.minutes.textContent);
